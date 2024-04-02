675bc04afd362b898a1517f6a7602f47d460c81d
Devika Reddy Kundavaram <133973609+DevikaReddyKundavaram@users.noreply.github.com>
2024-04-02 22:10:21 +0530

    Add files via upload

---
+++ toursim_app/static/toursim_app/js/index_script.js
@@ -0,0 +1,18 @@
+// Add JavaScript functionalities here
+
+// Example: Smooth scrolling for anchor links
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+// Add more JavaScript functionalities as needed
